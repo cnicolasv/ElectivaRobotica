a09d0bcd954ecd9f8e8a8b1e39f06e2ebe477489
Nicolas <cnicolas.villamil@gmail.com>
2024-09-22 01:24:40 -0500

    update

---
+++ taller_2/punto_2/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_punto_2_FILE C:/Users/cnico/Code/electiva_robotica/taller_2/punto_2/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/punto_2.exe)
+set(__QT_DEPLOY_TARGET_punto_2_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_punto_2_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
